7569cd47c5919149ef4c4b4be063da3eeabea394
Balaj Khan <balajkhan07@gmail.com>
2019-10-19 14:33:33 -0400

    monster rolodex github page deploy

---
+++ precache-manifest.faa411c844a982bfad73eb6e4949438d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7350755726c26be18a61617248e4e2f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a82d3ae8e8d75dbd53c6",
+    "url": "/monsters-rolodex/static/css/main.bd523985.chunk.css"
+  },
+  {
+    "revision": "1b93be959a299ad27afe",
+    "url": "/monsters-rolodex/static/js/2.81680560.chunk.js"
+  },
+  {
+    "revision": "a82d3ae8e8d75dbd53c6",
+    "url": "/monsters-rolodex/static/js/main.445ae9d6.chunk.js"
+  },
+  {
+    "revision": "e76d5e08d886c5e48de1",
+    "url": "/monsters-rolodex/static/js/runtime-main.8b9249a3.js"
+  }
+]);
